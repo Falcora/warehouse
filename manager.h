131ed9f09cef26b0b617c3254dc5d3cce1f99881
FernandoGomez <fernando85@msn.com>
2013-02-02 15:14:43 -0700

    Update manager.h
    Done.

---
+++ manager.h
@@ -0,0 +1,32 @@
+/*
+ * Manager class definitions.
+ *
+ * Fernando Gomez
+ */
+
+#ifndef MANAGER_H
+#define MANAGER_H
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include "item.cc"
+#include "date.cc"
+
+class manager
+{
+ public:
+  
+   
+
+ private:
+	 
+
+
+
+
+}
+
+#endif
