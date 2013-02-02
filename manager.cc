1b82abe8dca80fb5db7c0bf765e4ec6592f7fb38
FernandoGomez <fernando85@msn.com>
2013-02-02 13:48:23 -0700

    Update manager.cc
    added code for food, warehouse, date, and receive.

--- manager.cc
+++ manager.cc
@@ -1,6 +1,7 @@
 /* This class reads an input file and creates objects for items and warehouses.
  * Also, it calls functions on warehouses as stated by the input file.
  *
+ * Fernando Gomez
  */
 
 #include "manager.h"
@@ -8,6 +9,7 @@
 #include <stdlib.h>
 #include <iostream>
 #include <set>
+#include "warehouse.h"
 using namespace std;
 using namespace inventory;
 
@@ -21,7 +23,11 @@ int main(int argc, char* argv[]){
   ifstream inputfile(filename);
   string line;
   
-  set<item> items;			// Create a set to hold our items.
+		date startDate;
+	 set<item> items;				// Create a set to hold our items.
+	 set<warehouse> warehouses;	// Create a set to hold our warehouses.
+  
+  
   if(inputfile.is_open())
 	{
 	  while(inputfile.good())
@@ -53,9 +59,9 @@ int main(int argc, char* argv[]){
 	  for(std::vector<std::string>::iterator it = tokens.begin(); it != tokens.end(); ++it) 
 		{
 		  // here we check what the inital token is
-		  if(*it == "FoodItem" || food)
+		if(*it == "FoodItem" || food)
 		{
-		  if(food)
+			if(food)
 			{
 				++it;
 				++it;
@@ -76,34 +82,91 @@ int main(int argc, char* argv[]){
 			  // create a food item.
 			  item *food = new item(upc,2,name);
 			  // add the new food item to our hashset
+			  
 			  items.insert(food);
 			  break;
 			}
-		  food = true;
+		food = true;
 		}
-		  else if(*it == "Warehouse" || warehouse)
+		else if(*it == "Warehouse" || warehouse)
 		{
-		  // Create warehouse
-
-
+		  if(warehouse)
+		  {
+			  inventory::warehouse *house = new inventory::warehouse(line.substr(12));
+			  warehouses.insert(house);
+		  }
 		  warehouse = true;
 		}
 		  else if(*it == "Start" || start)
 		{
-		  // Set start date
+		  if(start)
+		  {
+			  // Get the date
+			  int month = atoi( line.substr(12,2).c_str());
+			  int day = atoi( line.substr(15,2).c_str());
+			  int year = atoi( line.substr(18,4).c_str());
 
+			  startDate.setDate(month, day, year);
+		  }
 		  start = true;
 		}
 		  else if(*it == "Receive" || receive)
 		{
 		  // call the receive function on the right warehouse
+			if(receive)
+			{
+				// First, get the UPC of the item we are receiving.
+				++it;
+				string reUPC = *it;
+				int receiveUPC = atoi(reUPC.c_str());
+				// Then get the quantity
+				++it;
+				string qu = *it;
+				int quantity = atoi(qu.c_str());
+				// Then the the name of the receiving warehouse
+				++it;
+				string receivingWarehouse = *it;
 
+				// Now find the warehouse in our set.
+				inventory::warehouse *receiveWarehouse = new inventory::warehouse("temp");
+				for(set<inventory::warehouse>::iterator houses = warehouses.begin(); houses != warehouses.end(); ++houses) 
+				{
+					inventory::warehouse tempHouse = *houses;
+					if(receivingWarehouse == tempHouse.name)
+					{
+						*receiveWarehouse = tempHouse;
+						break;
+					}
+				}
+
+				// Now figure out the expiration date
+				item *receiveItem = new item(0,0,"temp");
+				for(set<item>::iterator itt = items.begin(); itt != items.end(); ++itt) 
+				{
+					item temp = *itt;
+					if(receiveUPC == temp.getUPC())
+					{
+						*receiveItem = temp;
+						break;
+					}
+				}
+				int receiveItemShelfDays = receiveItem->getShelfLife();
+				date *expiration = new date();
+				for(int i = 0; i < receiveItemShelfDays; i++)
+				{
+					expiration->advanceDay();
+				}
+
+				// Now add the item to the warehouse
+
+				receiveWarehouse->itemReceive(receiveUPC,quantity,*expiration);	
+			}
 		  receive = true;
 		}
 		  else if(*it == "Next" || next)
 		{
 		  // call the next day function
-
+			
 		  next = true;
 		}
 		  else if(*it == "Request" || request)
